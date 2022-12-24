742321bf89f196819ba93dd404b64205f1128b8d
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-12-24 17:51:17 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Alexander Grishin on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
